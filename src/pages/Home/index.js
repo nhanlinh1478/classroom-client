420d5f42c72f2997388fa5cfeae6be3c9d102aa6
nhanlinh1478 <luongtrongnhan1082000@gmail.com>
2022-06-08 21:00:26 +0700

    update_testcase

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,6 +5,7 @@ import { useSelector } from 'react-redux'
 import LoadingPage from 'src/components/LoadingPage'
 ///test cicd
 //them testcase
+//est
 const Home = () => {
   const classrooms = useSelector((state) => state.classrooms.classrooms)
   const isLoading = useSelector((state) => state.classrooms.isLoading)
